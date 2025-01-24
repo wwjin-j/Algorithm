df957d8bee9d3a2fa53051474ddae01b7681c845
wwjin-j <96734122+wwjin-j@users.noreply.github.com>
2025-01-24 17:26:19 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 312 ms, Memory: 66216 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,28 @@
+"""
+이진검색 트리
+"""
+
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+n = int(input())
+graph = [[] for _ in range(n+1)]
+visited = [False]*(n+1)
+
+def dfs(n):
+    for i in graph[n]:
+        if not visited[i]:
+            visited[i] = n
+            dfs(i)
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited[1] = 1
+dfs(1)
+
+for k in visited[2:]:
+    print(k)
